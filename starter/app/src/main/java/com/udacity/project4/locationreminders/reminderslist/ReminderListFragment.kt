0de3eff1cc2bd1800c1dae32f17ad09100b0c008
kevin.claassen <claassenk@outlook.com>
2021-02-26 23:48:35 +0100

    Added Logout function Current issue with login after logout

--- starter/app/src/main/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragment.kt
+++ starter/app/src/main/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragment.kt
@@ -1,9 +1,14 @@
 package com.udacity.project4.locationreminders.reminderslist
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.*
 import androidx.databinding.DataBindingUtil
+import com.firebase.ui.auth.AuthUI
+import com.google.android.material.snackbar.Snackbar
 import com.udacity.project4.R
+import com.udacity.project4.authentication.AuthenticationActivity
 import com.udacity.project4.base.BaseFragment
 import com.udacity.project4.base.NavigationCommand
 import com.udacity.project4.databinding.FragmentRemindersBinding
@@ -71,7 +76,23 @@ class ReminderListFragment : BaseFragment() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.logout -> {
-//                TODO: add the logout implementation
+                // Go back to Login screen
+                AuthUI.getInstance()
+                    .signOut(requireContext())
+                    .addOnCompleteListener {
+            if (it.isSuccessful) {
+                val activity = requireActivity()
+                startActivity(Intent(activity, AuthenticationActivity::class.java))
+                activity.finish()
+            } else {
+            // Show some message to the user.
+                Snackbar.make(
+                        requireView(),
+                        getString(R.string.logout_failed),
+                        Snackbar.LENGTH_SHORT
+                ).show()
+        }
+    }
             }
         }
         return super.onOptionsItemSelected(item)
